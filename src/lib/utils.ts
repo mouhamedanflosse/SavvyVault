78d4b72794df72e354ea31d0b6f6f1381e7a79b5
mouhamed anflouss <mouhamedanflosse@gmail.com>
2024-10-26 23:01:21 +0100

    fix import causing an error 🛠

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,6 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
-import { iconsSvg, MicrosoftIcons } from "./iconsSvg"
+import { iconsSvg } from "./iconsSvg"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
