f1ac6ab1e4e29cda806ec1283849ab16331b57e1
mouhamed anflouss <mouhamedanflosse@gmail.com>
2024-10-26 15:14:30 +0100

    add a loader for the documents page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,47 @@
-'use client'
-import {  useOrganization } from "@clerk/nextjs";
+"use client";
+import { useOrganization } from "@clerk/nextjs";
 import { Authenticated, useQuery } from "convex/react";
 import { api } from "../../convex/_generated/api";
 import MaxWidthWrapper from "@afs/components/ui/MaxWithWrapper";
 import { UploadDoc } from "@afs/components/custom/UploadFiele";
 import Lottie from "lottie-react";
-import sleepyCat from "../../public/assets/sleepy_cat.json"
-import sleepy_raccoon from "../../public/assets/sleepy_raccoon.json"
-import orange_sleepy_cat from "../../public/assets/orange_sleepy_cat.json"
+import sleepyCat from "../../public/assets/sleepy_cat.json";
+import sleepy_raccoon from "../../public/assets/sleepy_raccoon.json";
+import orange_sleepy_cat from "../../public/assets/orange_sleepy_cat.json";
 import { Document } from "@afs/components/custom/DocCard";
+import { Loader2 } from "lucide-react";
 
 function App() {
+  const { organization } = useOrganization();
+  const Docs = useQuery(api.document.getDocuments, { orgId: organization?.id });
 
-  const {organization} = useOrganization()
-  const Docs = useQuery(api.document.getDocuments , {orgId : organization?.id } )
-
-  
   return (
-    <MaxWidthWrapper>
-    <main className="flex min-h-screen flex-col items-center gap-14 p-24">
-    <div className="flex justify-end w-full">
-      <Authenticated>
-        <UploadDoc editMode={false} />
-      </Authenticated>
-    </div>
-    {Docs ?
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-         {Docs?.map((doc : any) => {
-          return <Document key={doc._id} doc={doc} />
-        }) }
+    <MaxWidthWrapper>{ Docs == undefined ?  <Loader2 className="animate-spin text-3xl w-20 h-20 mx-auto mt-36" /> :
+      <main className="flex min-h-screen flex-col items-center gap-14 p-24">
+        <div className="flex w-full justify-end">
+          <Authenticated>
+            <UploadDoc editMode={false} />
+          </Authenticated>
         </div>
-       : <div>
-         <Lottie animationData={orange_sleepy_cat} loop={true} className="mx-auto w-36" />
-        </div>
-         }
-    </main>
+
+        {Docs.length ? (
+          <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {/* use length to handl empty docs page , and null to handl loading */}
+            {/* rememberalso fix the mobile view  */}
+            {Docs?.map((doc: any) => {
+              return <Document key={doc._id} doc={doc} />;
+            })}
+          </div>
+        ) : (
+          <div>
+            <Lottie
+              animationData={orange_sleepy_cat}
+              loop={true}
+              className="mx-auto w-36"
+            />
+          </div>
+        )}
+      </main>}
     </MaxWidthWrapper>
   );
 }
