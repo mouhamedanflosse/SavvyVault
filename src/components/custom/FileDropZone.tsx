c21e96dc166d2b54770fa5165d56113a73f86df6
mouhamed anflouss <mouhamedanflosse@gmail.com>
2024-10-27 22:22:44 +0100

    add a image preview for uploaded files 🛠🍖✅

--- src/components/custom/FileDropZone.tsx
+++ src/components/custom/FileDropZone.tsx
@@ -3,7 +3,7 @@
 import * as React from "react"
 import Image from "next/image"
 // import { Cross2Icon, FileTextIcon, UploadIcon } from "@radix-ui/react-icons"
-import { Upload } from 'lucide-react';
+import { FileTextIcon, Upload, X } from 'lucide-react';
 import Dropzone, {
   type DropzoneProps,
   type FileRejection,
@@ -290,6 +290,91 @@ export function FileUploader(props: FileUploaderProps) {
           </div>
         )}
       </Dropzone>
+      {files?.length ? (
+        <div className="h-fit w-full px-3">
+          <div className="flex max-h-48 flex-col gap-4">
+            {files?.map((file, index) => (
+              <FileCard
+                key={index}
+                file={file}
+                onRemove={() => onRemove(index)}
+                // progress={progresses?.[file.name]}
+              />
+            ))}
+          </div>
+        </div>
+      ) : null}
     </div>
   )
+
+
+  
+interface FileCardProps {
+  file: File
+  onRemove: () => void
+  progress?: number
+}
+
+function FileCard({ file, onRemove }: FileCardProps) {
+  return (
+    <div className="relative flex items-center gap-2.5">
+      <div className="flex flex-1 gap-2.5">
+        {isFileWithPreview(file) ? <FilePreview file={file} /> : null}
+        <div className="flex w-full flex-col gap-2">
+          <div className="flex flex-col gap-px">
+            <p className="line-clamp-1 text-sm font-medium text-foreground/80">
+              {file.name}
+            </p>
+            <p className="text-xs text-muted-foreground">
+              {formatBytes(file.size)}
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="flex items-center gap-2">
+        <Button
+          type="button"
+          variant="outline"
+          size="icon"
+          className="size-7"
+          onClick={onRemove}
+        >
+          <X className="size-4" aria-hidden="true" />
+          <span className="sr-only">Remove file</span>
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+function isFileWithPreview(file: File): file is File & { preview: string } {
+  return "preview" in file && typeof file.preview === "string"
+}
+
+interface FilePreviewProps {
+  file: File & { preview: string }
+}
+
+function FilePreview({ file }: FilePreviewProps) {
+  if (file.type.startsWith("image/")) {
+    return (
+      <Image
+        src={file.preview}
+        alt={file.name}
+        width={48}
+        height={48}
+        loading="lazy"
+        className="aspect-square shrink-0 rounded-md object-cover"
+      />
+    )
+  }
+
+  return (
+    <FileTextIcon
+      className="size-10 text-muted-foreground"
+      aria-hidden="true"
+    />
+  )
+}
+
 }
