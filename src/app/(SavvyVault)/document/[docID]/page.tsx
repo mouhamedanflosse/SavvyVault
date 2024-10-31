5bce3a3a15520cdfb2214fb3407ca6eea7349521
mouhamed anflouss <mouhamedanflosse@gmail.com>
2024-10-31 23:57:39 +0100

    add loader for the documents page 🛠💡

--- src/app/(SavvyVault)/document/[docID]/page.tsx
+++ src/app/(SavvyVault)/document/[docID]/page.tsx
@@ -15,6 +15,7 @@ import {
   BreadcrumbSeparator,
 } from "@afs/components/ui/breadcrumb";
 import Link from "next/link";
+import { Loader2 } from "lucide-react";
 
 export default function Document({
   params,
@@ -30,34 +31,34 @@ export default function Document({
     orgId: organization?.id,
   });
 
-  if (!doc) {
-    return (
-      <ContentLayout title="restricted">
-        <Breadcrumb>
-          <BreadcrumbList>
-            <BreadcrumbItem>
-              <BreadcrumbLink asChild>
-                <Link href="/">Home</Link>
-              </BreadcrumbLink>
-            </BreadcrumbItem>
-            <BreadcrumbSeparator />
-            <BreadcrumbItem>
-              <BreadcrumbLink asChild>
-                <Link href='/dashboard'>Dashboard</Link>
-              </BreadcrumbLink>
-            </BreadcrumbItem>
-            <BreadcrumbSeparator />
-          </BreadcrumbList>
-        </Breadcrumb>
-        <h1 className="mt-32 text-center text-lg">
-          You cannot view this content in
-          {!organization
-            ? "personal mode. Please switch to the correct organization."
-            : "this organization. Please switch to the correct organization or your personal account."}
-        </h1>
-      </ContentLayout>
-    );
-  }
+  // if (!doc) {
+  //   return (
+  //     <ContentLayout title="restricted">
+  //       <Breadcrumb>
+  //         <BreadcrumbList>
+  //           <BreadcrumbItem>
+  //             <BreadcrumbLink asChild>
+  //               <Link href="/">Home</Link>
+  //             </BreadcrumbLink>
+  //           </BreadcrumbItem>
+  //           <BreadcrumbSeparator />
+  //           <BreadcrumbItem>
+  //             <BreadcrumbLink asChild>
+  //               <Link href="/dashboard">Dashboard</Link>
+  //             </BreadcrumbLink>
+  //           </BreadcrumbItem>
+  //           <BreadcrumbSeparator />
+  //         </BreadcrumbList>
+  //       </Breadcrumb>
+  //       <h1 className="mt-32 text-center text-lg">
+  //         You cannot view this content in
+  //         {!organization
+  //           ? "personal mode. Please switch to the correct organization."
+  //           : "this organization. Please switch to the correct organization or your personal account."}
+  //       </h1>
+  //     </ContentLayout>
+  //   );
+  // }
 
   return (
     <ContentLayout title="dashboard">
@@ -76,23 +77,27 @@ export default function Document({
           </BreadcrumbItem>
           <BreadcrumbSeparator />
           <BreadcrumbItem>
-            <BreadcrumbPage >{doc.name}</BreadcrumbPage>
+            <BreadcrumbPage>{doc?.name}</BreadcrumbPage>
           </BreadcrumbItem>
         </BreadcrumbList>
       </Breadcrumb>
-      <div className="flex flex-col gap-4 mt-6">
-        <div className="flex w-full justify-start">
-          <h1>{doc.name}</h1>
+      {doc == undefined ? (
+        <Loader2 className="mx-auto mt-36 h-20 w-20 animate-spin text-3xl" />
+      ) : (
+        <div className="mt-6 flex flex-col gap-4">
+          <div className="flex w-full justify-start">
+            <h1>{doc.name}</h1>
+          </div>
+          <div className="flex w-full gap-6">
+            <ScrollArea className="h-[500px] w-1/2 overflow-scroll rounded-md border bg-gray-900 p-4">
+              {doc.docURL && (
+                <iframe className="block h-[500px] w-full" src={doc.docURL} />
+              )}
+            </ScrollArea>
+            <ChatBox docId={params.docID} />
+          </div>
         </div>
-        <div className="flex w-full gap-6">
-          <ScrollArea className="h-[500px] w-1/2 overflow-scroll rounded-md border bg-gray-900 p-4">
-            {doc.docURL && (
-              <iframe className="block h-[500px] w-full" src={doc.docURL} />
-            )}
-          </ScrollArea>
-          <ChatBox docId={params.docID} />
-        </div>
-      </div>
+      )}
     </ContentLayout>
   );
 }
